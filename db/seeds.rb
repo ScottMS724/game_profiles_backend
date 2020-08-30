96698e39b0c894cbb4242adde1adc6147a232073
scottms724 <scottms724@gmail.com>
2020-08-30 12:23:21 -0400

    genre model, seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+fps = Genre.create(name: FPS)
+mmorpg = Genre.create(name: MMORPG)
+rts = Genre.create(name: RTS)
+adventure = Genre.create(name: Adventure)
