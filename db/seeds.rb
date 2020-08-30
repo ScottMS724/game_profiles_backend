8d802ac4b552c363ea756ee956603337eec24df4
scottms724 <scottms724@gmail.com>
2020-08-30 12:56:38 -0400

    tested associations

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,9 @@
-fps = Genre.create(name: FPS)
-mmorpg = Genre.create(name: MMORPG)
-rts = Genre.create(name: RTS)
-adventure = Genre.create(name: Adventure)
+Genre.destroy_all
+Game.destroy_all
 
-Game.create(title: "World of Warcraft - Classic", image_url: "https://preview.redd.it/2ckid2re2ny21.jpg?width=1920&format=pjpg&auto=webp&s=c6ce82a2766bde671a29ce5abe3ab3bccc232105", review: "Very fun. Can raid with 40 people. Easily can sink many hours into playing this game.", genre_id: mmorpg.id)
+fps = Genre.create(name: "FPS")
+mmorpg = Genre.create(name: "MMORPG")
+rts = Genre.create(name: "RTS")
+adventure = Genre.create(name: "Adventure")
+
+Game.create(title: "World of Warcraft - Classic", image_url: "https://preview.redd.it/2ckid2re2ny21.jpg?width=1920&format=pjpg&auto=webp&s=c6ce82a2766bde671a29ce5abe3ab3bccc232105", review: "Very fun. Can raid with 40 people. Easily can sink many hours into playing this game.", rating: 8, genre_id: mmorpg.id)
